1b34a4d2fbae0230f7c4a3e5f0711fef24e0bfde
leo4410 <l02.seminatore@gmail.com>
2025-02-03 22:06:40 +0100

    add first placeholder version

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import fhgruempeliLogo from './assets/Logo-Standard.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={fhgruempeliLogo} className="logo react" alt="React logo" height={window.innerHeight} style={{ alignSelf: 'center' }} />
+    </>
+  )
+}
+
+export default App
